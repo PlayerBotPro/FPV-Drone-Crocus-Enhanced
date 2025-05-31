9dc4fb8a35bc1286a8adcbe1d6f94ab6326115a2
PlayerBot <playerbotpro@hotmail.com>
2025-05-31 22:41:37 +0800

    load other configs in config.cpp

--- config.cpp
+++ config.cpp
@@ -5,11 +5,14 @@ class CfgPatches {
 		url="";
 		requiredAddons[]= {"ArmaFPV_Data"};
 		requiredVersion=0.1;
+		// add CfgVehicles to units[]
 		units[]= {"FPV_Retranslator"};
 		weapons[]={};
 	};
 };
 
 #include "CfgAmmo.hpp"
-#include "CfgVehicles.hpp"
 #include "CfgFunctions.hpp"
+#include "CfgMagazines.hpp"
+#include "CfgVehicles.hpp"
+#include "CfgWeapons.hpp"
