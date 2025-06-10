13cb4df72276695cbb37f347480142345ae77f7d
PlayerBot <playerbotpro@hotmail.com>
2025-06-10 21:13:01 +0800

    remove unused item class

--- addons/BOT_ArmaFPV_Enhanced/CfgVehicles.hpp
+++ addons/BOT_ArmaFPV_Enhanced/CfgVehicles.hpp
@@ -11,8 +11,10 @@ class CfgVehicles {
 	class B_Crocus_AP;
 	class O_Crocus_AT;
 	class BOT_vehicle_B_FPV_AT: B_Crocus_AT {
+		displayName = "FPV AT";
 	};
 	class BOT_vehicle_B_FPV_AP: B_Crocus_AP {
+		displayName = "FPV AP";
 	};
 	class BOT_vehicle_FPV_Fiber: O_Crocus_AT {
 		//inherent from O_Crocus_AT?
@@ -24,21 +26,22 @@ class CfgVehicles {
 		//inherent from O_Crocus_AT?
 	};
 
+	// currently not needed for throwable FPVs
 	// Item (Ground weapon holder)
-	class Item_Base_F;
-	// maybe add a base class
-	class Bot_Item_FPV_xx: Item_Base_F {
-		scope = 2;
-		scopeCurator = 2;
-		displayName = "$STR_Mavic_Item_Mavic3_displayName";
-		author = "$STR_Mavic_Author";
-		vehicleClass = "Items";
-		class TransportItems {
-			// make sure its CfgWeapons class?
-			class _xx_BOT_weapon_ItemFPV_1 {
-				name = "BOT_weapon_ItemFPV_1";
-				count = 1;
-			};
-		};
-	};
+	// class Item_Base_F;
+	// // maybe add a base class
+	// class Bot_Item_FPV_xx: Item_Base_F {
+	// 	scope = 2;
+	// 	scopeCurator = 2;
+	// 	displayName = "$STR_Mavic_Item_Mavic3_displayName";
+	// 	author = "$STR_Mavic_Author";
+	// 	vehicleClass = "Items";
+	// 	class TransportItems {
+	// 		// make sure its CfgWeapons class?
+	// 		class _xx_BOT_weapon_ItemFPV_1 {
+	// 			name = "BOT_weapon_ItemFPV_1";
+	// 			count = 1;
+	// 		};
+	// 	};
+	// };
 };
