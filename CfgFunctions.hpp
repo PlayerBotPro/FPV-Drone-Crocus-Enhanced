5e765f3ee19e27759b684373a26b14e8cf94f8a7
PlayerBot <playerbotpro@hotmail.com>
2025-06-10 16:46:01 +0800

    comment warhead change

--- CfgFunctions.hpp
+++ CfgFunctions.hpp
@@ -1,14 +1,14 @@
 class CfgFunctions {
 	// Overwrite function from original mod
-	class DB {
-		// you can change FPV to any name you want??
-		class FPV {
-            // need change file path
-			// BIKI: The file attribute can override the category's loading path segment - it is required for mods.
-			file="\ArmaFPV\functions";
-			class fpv_onDestroy {};
-		};
-	};
+	// class DB {
+	// 	// you can change FPV to any name you want??
+	// 	class FPV {
+    //         // need change file path
+	// 		// BIKI: The file attribute can override the category's loading path segment - it is required for mods.
+	// 		file="\ArmaFPV\functions";
+	// 		class fpv_onDestroy {};
+	// 	};
+	// };
 
 	class BOT {
 		class aCategoryName {
