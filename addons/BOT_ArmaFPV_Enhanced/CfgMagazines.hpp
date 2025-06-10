67233c0edd1a20111b40f7a2d7ebf81f05fb1653
PlayerBot <playerbotpro@hotmail.com>
2025-06-10 20:39:58 +0800

    change display name

--- addons/BOT_ArmaFPV_Enhanced/CfgMagazines.hpp
+++ addons/BOT_ArmaFPV_Enhanced/CfgMagazines.hpp
@@ -9,14 +9,15 @@ class CfgMagazines {
     };
     class BOT_magazine_fpv_AT_muzzle: BOT_magazine_fpv_throwable_base {
         scope = 2;
-		displayName = "$STR_Mavic_Drop_Grenade_M67_displayName";
-		displayNameShort = "$STR_Mavic_Drop_Grenade_M67_displayShort";
-		descriptionShort = "$STR_Mavic_Drop_Grenade_M67_description";
+		displayName = "BOT FPV AT test";
+		displayNameShort = "BOT FPV AT test short";
+		descriptionShort = "BOT FPV AT test descriptionShort";
 		picture = "\A3\Weapons_F\Data\UI\gear_M67_CA.paa";
 		model = "\A3\Weapons_f\ammo\Handgrenade";
 		ammo = "BOT_ammo_fpv_AT_muzzle";
     };
     class BOT_magazine_fpv_AP_muzzle: BOT_magazine_fpv_AT_muzzle {
+        displayName = "BOT FPV AP test";
         ammo = "BOT_ammo_fpv_AP_muzzle";
     };
 }
