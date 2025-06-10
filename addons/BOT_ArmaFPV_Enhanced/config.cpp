b15a5d9127c8c9ae287fc7e9e1e8b16b045d6d01
PlayerBot <playerbotpro@hotmail.com>
2025-06-10 21:13:17 +0800

    add units in cfgPatches

--- addons/BOT_ArmaFPV_Enhanced/config.cpp
+++ addons/BOT_ArmaFPV_Enhanced/config.cpp
@@ -6,7 +6,7 @@ class CfgPatches {
 		requiredAddons[]= {"ArmaFPV_Data"};
 		requiredVersion=0.1;
 		// add CfgVehicles to units[]
-		units[]= {"FPV_Retranslator"};
+		units[]= {"FPV_Retranslator","BOT_vehicle_B_FPV_AT","BOT_vehicle_B_FPV_AP","BOT_vehicle_FPV_Fiber","BOT_vehicle_FPV_TI","BOT_vehicle_B_FPV_xx"};
 		weapons[]={};
 	};
 };
