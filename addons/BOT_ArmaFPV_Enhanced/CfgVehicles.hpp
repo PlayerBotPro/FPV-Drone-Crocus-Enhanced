3d9585a753e4a7010fb22ca42c16a47782faa99e
PlayerBot <playerbotpro@hotmail.com>
2025-06-11 19:55:04 +0800

    FPV TI classes in CfgVehicles

--- addons/BOT_ArmaFPV_Enhanced/CfgVehicles.hpp
+++ addons/BOT_ArmaFPV_Enhanced/CfgVehicles.hpp
@@ -7,7 +7,7 @@ class CfgVehicles {
 	};
 
 	// The real flying FPV
-class drone_base_F;
+	class drone_base_F;
 	class B_Crocus_AT: drone_base_F {
 		class ViewOptics;
 	};
@@ -66,6 +66,70 @@ class drone_base_F;
 		crew = "C_UAV_AI_F";
 	};
 
+	// Thermal Imagery
+	class BOT_vehicle_FPV_AT_TI_base: B_Crocus_AT {
+		displayName = "FPV AT TI";
+		scope = 1;
+		scopeCurator = 1;
+		class ViewOptics: ViewOptics {
+			visionMode[] = {
+				"Normal",
+				"TI"
+			};
+			// thermalMode[] = {0,1};
+		};
+	};
+	class BOT_vehicle_FPV_AP_TI_base: B_Crocus_AP {
+		displayName = "FPV AP TI";
+		class ViewOptics: ViewOptics {
+			visionMode[] = {
+				"Normal",
+				"TI"
+			};
+			// thermalMode[] = {0,1};
+		};
+
+	};
+	
+	class BOT_vehicle_B_FPV_AT_TI: BOT_vehicle_FPV_AT_TI_base {
+		scope = 2;
+		scopeCurator = 2;
+	};
+	class BOT_vehicle_B_FPV_AP_TI: BOT_vehicle_FPV_AP_TI_base {
+		scope = 2;
+		scopeCurator = 2;
+	};
+	class BOT_vehicle_O_FPV_AT_TI: BOT_vehicle_B_FPV_AT_TI {
+		side = 0;
+		faction="OPF_F";
+		crew="O_UAV_AI_F";
+	};
+	class BOT_vehicle_O_FPV_AP_TI: BOT_vehicle_B_FPV_AP_TI {
+		side = 0;
+		faction="OPF_F";
+		crew="O_UAV_AI_F";
+	};
+	class BOT_vehicle_I_FPV_AT_TI: BOT_vehicle_B_FPV_AT_TI {
+		side = 2;
+		faction="IND_F";
+		crew="I_UAV_AI_F";
+	};
+	class BOT_vehicle_I_FPV_AP_TI: BOT_vehicle_B_FPV_AP_TI {
+		side = 2;
+		faction="IND_F";
+		crew="I_UAV_AI_F";
+	};
+	class BOT_vehicle_C_FPV_AT_TI: BOT_vehicle_B_FPV_AT_TI {
+		side = 3;
+		faction = "CIV_F";
+		crew = "C_UAV_AI_F";
+	};
+	class BOT_vehicle_C_FPV_AP_TI: BOT_vehicle_B_FPV_AP_TI {
+		side = 3;
+		faction = "CIV_F";
+		crew = "C_UAV_AI_F";
+	};
+	
 	class BOT_vehicle_B_FPV_AT: B_Crocus_AT {
 		displayName = "FPV AT";
 	};
