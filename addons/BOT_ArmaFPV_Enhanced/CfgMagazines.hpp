eb2ed54cbf2589fa761fe2c31f1d611e5beea12a
PlayerBot <playerbotpro@hotmail.com>
2025-06-11 10:06:03 +0800

    change displayNames

--- addons/BOT_ArmaFPV_Enhanced/CfgMagazines.hpp
+++ addons/BOT_ArmaFPV_Enhanced/CfgMagazines.hpp
@@ -9,17 +9,19 @@ class CfgMagazines {
     };
     class BOT_magazine_fpv_AT_muzzle: BOT_magazine_fpv_throwable_base {
         scope = 2;
-		displayName = "BOT FPV AT test";
-		displayNameShort = "BOT FPV AT test short";
-		descriptionShort = "BOT FPV AT test descriptionShort";
+		displayName = "FPV AT";
+		displayNameShort = "FPV AT";
+		descriptionShort = "Throw me out to fly";
 		picture = "\ArmaFPV\data\krokus1.jpg";
         model = "\ArmaFPV\drone.p3d";
 		ammo = "BOT_ammo_fpv_AT_muzzle";
     };
     class BOT_magazine_fpv_AP_muzzle: BOT_magazine_fpv_AT_muzzle {
-        displayName = "BOT FPV AP test";
-        ammo = "BOT_ammo_fpv_AP_muzzle";
+        displayName = "FPV AP";
+		displayNameShort = "FPV AP";
+		descriptionShort = "Throw me out to fly";
         picture = "\ArmaFPV\data\termo.jpg";
 		model = "\ArmaFPV\drone2\drone2.p3d";
+        ammo = "BOT_ammo_fpv_AP_muzzle";
     };
 };
