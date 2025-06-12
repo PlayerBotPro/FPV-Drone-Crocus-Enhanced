72f6d906977e0c86ec831133ac7dc7c9173128de
PlayerBot <playerbotpro@hotmail.com>
2025-06-12 10:12:47 +0800

    prepare CfgAmmo using init EH and submunition

--- addons/BOT_ArmaFPV_Enhanced/CfgAmmo.hpp
+++ addons/BOT_ArmaFPV_Enhanced/CfgAmmo.hpp
@@ -30,6 +30,108 @@ class CfgAmmo {
 	// FPV throwable ammo
 	// The Real job: how to convert to a actual vehicle FPV from ammo? RF CDLC! 
 	class GrenadeHand;
+
+	// Base class for model
+	class BOT_ammo_FPV_AT_fake_base: GrenadeHand {
+		// https://community.bistudio.com/wiki/Arma_3:_Weapon_Config_Guidelines#Ammo_changes_on_fly_and_on_hit
+		simulation = "shotSubmunitions";
+		triggerTime = 1;
+		deleteParentWhenTriggered = true;
+		triggerSpeedCoef = 0;
+		submunitionParentSpeedCoef = 1;
+		submunitionDirectionType = "SubmunitionModelDirection";
+		submunitionConeAngle = 0;
+		submunitionConeAngleHorizontal = 0;
+		submunitionConeType[] = { "custom", { { 0, 0 } } };
+		//
+
+		model = "\ArmaFPV\drone.p3d";
+	};
+	class BOT_ammo_FPV_AP_fake_base: BOT_ammo_FPV_AT_fake_base {
+		model = "\ArmaFPV\drone2\drone2.p3d";
+	};
+	// another base class for Eventhandlers
+	class BOT_ammo_FPV_base: GrenadeHand {
+		class Eventhandlers {
+			init = "_this call BOT_fnc_fpv_deploy";
+		};
+	};
+
+	// low definition FPVs, from original ArmaFPV mod
+	// Fake ammo for ACE throw
+	class BOT_ammo_FPV_AT_LD_fake: BOT_ammo_FPV_AT_fake_base {
+		submunitionAmmo = "BOT_ammo_FPV_AT_LD";
+	};
+	class BOT_ammo_FPV_AP_LD_fake: BOT_ammo_FPV_AP_fake_base {
+		submunitionAmmo = "BOT_ammo_FPV_AP_LD";
+	};
+	// real ammo class. If everything is proper, when this class shows up, the vehicle should be spawned
+	class BOT_ammo_FPV_AT_LD: BOT_ammo_FPV_base {
+		BOT_vehicleSide[] = {
+			"B_Crocus_AT",
+			"O_Crocus_AT",
+			"I_Crocus_AT",
+			// origin mod didnt have C_Crocus_AT
+			"B_Crocus_AT"
+		};
+	};
+	class BOT_ammo_FPV_AP_LD: BOT_ammo_FPV_base {
+		BOT_vehicleSide[] = {
+			"B_Crocus_AP",
+			"O_Crocus_AP",
+			"I_Crocus_AP",
+			"B_Crocus_AP"
+		};
+	};
+
+	// high definition FPVs, new added
+	class BOT_ammo_FPV_AT_HD_fake: BOT_ammo_FPV_AT_fake_base {
+		submunitionAmmo = "BOT_ammo_FPV_AT_HD";
+	};
+	class BOT_ammo_FPV_AP_HD_fake: BOT_ammo_FPV_AP_fake_base {
+		submunitionAmmo = "BOT_ammo_FPV_AP_HD";
+	};
+	class BOT_ammo_FPV_AT_HD: BOT_ammo_FPV_base {
+		BOT_vehicleSide[] = {
+			"BOT_vehicle_B_FPV_AT_HD",
+			"BOT_vehicle_O_FPV_AT_HD",
+			"BOT_vehicle_I_FPV_AT_HD",
+			"BOT_vehicle_C_FPV_AT_HD"
+		};
+	};
+	class BOT_ammo_FPV_AP_HD: BOT_ammo_FPV_base {
+		BOT_vehicleSide[] = {
+			"BOT_vehicle_B_FPV_AP_HD",
+			"BOT_vehicle_O_FPV_AP_HD",
+			"BOT_vehicle_I_FPV_AP_HD",
+			"BOT_vehicle_C_FPV_AP_HD"
+		};
+	};
+
+	// thermal imagery FPVs, new added
+	class BOT_ammo_FPV_AT_TI_fake: BOT_ammo_FPV_AT_fake_base {
+		submunitionAmmo = "BOT_ammo_FPV_AT_TI";
+	};
+	class BOT_ammo_FPV_AP_TI_fake: BOT_ammo_FPV_AP_fake_base {
+		submunitionAmmo = "BOT_ammo_FPV_AP_TI";
+	};
+	class BOT_ammo_FPV_AT_TI: BOT_ammo_FPV_base {
+		BOT_vehicleSide[] = {
+			"BOT_vehicle_B_FPV_AT_TI",
+			"BOT_vehicle_O_FPV_AT_TI",
+			"BOT_vehicle_I_FPV_AT_TI",
+			"BOT_vehicle_C_FPV_AT_TI"
+		};
+	};
+	class BOT_ammo_FPV_AP_TI: BOT_ammo_FPV_base {
+		BOT_vehicleSide[] = {
+			"BOT_vehicle_B_FPV_AP_TI",
+			"BOT_vehicle_O_FPV_AP_TI",
+			"BOT_vehicle_I_FPV_AP_TI",
+			"BOT_vehicle_C_FPV_AP_TI"
+		};
+	};
+
 	class BOT_ammo_fpv_AT_muzzle: GrenadeHand {
 		model = "\ArmaFPV\drone.p3d";
 		explosionTime = -1;
