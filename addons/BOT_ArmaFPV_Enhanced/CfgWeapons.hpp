189a7fac2002fec9c2755832ec6a9548aed06cd8
PlayerBot <playerbotpro@hotmail.com>
2025-06-12 10:12:00 +0800

    prepare CfgWeapons

--- addons/BOT_ArmaFPV_Enhanced/CfgWeapons.hpp
+++ addons/BOT_ArmaFPV_Enhanced/CfgWeapons.hpp
@@ -27,11 +27,39 @@ class CfgWeapons {
     //throwables to fly a UAV
     class GrenadeLauncher;
     class Throw: GrenadeLauncher {
+        // add new muzzle
         Muzzles[] += {
+            "BOT_weapon_FPV_AT_LD",
+            "BOT_weapon_FPV_AP_LD",
+            "BOT_weapon_FPV_AT_HD",
+            "BOT_weapon_FPV_AP_HD",
+            "BOT_weapon_FPV_AT_TI",
+            "BOT_weapon_FPV_AP_TI",
             "BOT_weapon_fpv_AT_muzzle",
             "BOT_weapon_fpv_AP_muzzle"
         };
         class ThrowMuzzle: GrenadeLauncher {};
+
+        // I dont know why but other mods do this
+		class BOT_weapon_FPV_AT_LD: ThrowMuzzle {
+            magazines[] = {"BOT_magazine_FPV_AT_LD"};
+        };
+		class BOT_weapon_FPV_AP_LD: ThrowMuzzle {
+            magazines[] = {"BOT_magazine_FPV_AP_LD"};
+        };
+		class BOT_weapon_FPV_AT_HD: ThrowMuzzle {
+            magazines[] = {"BOT_magazine_FPV_AT_HD"};
+        };
+		class BOT_weapon_FPV_AP_HD: ThrowMuzzle {
+            magazines[] = {"BOT_magazine_FPV_AP_HD"};
+        };
+		class BOT_weapon_FPV_AT_TI: ThrowMuzzle {
+            magazines[] = {"BOT_magazine_FPV_AT_TI"};
+        };
+		class BOT_weapon_FPV_AP_TI: ThrowMuzzle {
+            magazines[] = {"BOT_magazine_FPV_AP_TI"};
+        };
+
 		class BOT_weapon_fpv_AT_muzzle: ThrowMuzzle {
             // CfgMagazines class name
             magazines[] = {"BOT_magazine_fpv_AT_muzzle"};
