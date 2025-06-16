a651194af0bec3487370937d85c64dd9eedd0df2
PlayerBot <playerbotpro@hotmail.com>
2025-06-16 21:43:39 +0800

    I forgot that fpv_deploy should be used

--- addons/addon01/CfgFunctions.hpp
+++ addons/addon01/CfgFunctions.hpp
@@ -17,10 +17,10 @@ class CfgFunctions {
 			// file = should use path in $PREFIX$
 			file = "x\BOT_FPV_Enhanced\addons\addon01\functions";
 			
-			// these two not used
-			// class fpv_deploy {}; //BOT_fnc_fpv_deploy
-			// class fpv_killedEH {}; //
+			class fpv_deploy {}; //BOT_fnc_fpv_deploy
 			class fpv_handleDamage {}; //BOT_fnc_fpv_handleDamage
+			// this one not used
+			// class fpv_killedEH {}; //
 		};
 	};
 };
